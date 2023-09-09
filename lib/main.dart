2f858e7352e18786951790fe3c0739720575aad5
Jessica Goncalves <jessicasilgoncalves@hotmail.com>
2023-09-09 00:07:08 -0300

    atualizaçao na main

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 192, 230, 23)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Projeto Demo Flutter da Jessy')
     );
   }
 }
@@ -106,11 +106,12 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Esse é o numero de pessoas que fizeram checkin:',
+              style: TextStyle(color: Color.fromARGB(255, 2, 71, 21), fontSize: 15)
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headlineLarge,
             ),
           ],
         ),
@@ -118,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.library_add_check),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
